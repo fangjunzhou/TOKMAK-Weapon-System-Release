61ee365c98dbe89a04b71db7cbd1cb031d895aac
Fangjun Zhou <fangjun.zhou02@gmail.com>
2022-01-04 16:22:57 -0600

    Added: IWeaponManager and IWeapon interface Added some basic interface callback functions.

---
+++ Runtime/IWeapon.cs
@@ -0,0 +1,70 @@
+﻿using System.Threading.Tasks;
+
+namespace FinTOKMAK.WeaponSystem.Runtime
+{
+    /// <summary>
+    /// The interface for Weapon class
+    /// </summary>
+    public interface IWeapon
+    {
+        #region Property
+
+        /// <summary>
+        /// The unique id of the weapon.
+        /// </summary>
+        string id { get; }
+
+        #endregion
+
+        #region Weapon Logic Callback
+
+        /// <summary>
+        /// The method called when the weapon is initialized for the first time.
+        /// </summary>
+        void OnInitialize();
+
+        /// <summary>
+        /// The method is called each Unity Update when the weapon is put out and able to use.
+        /// </summary>
+        void OnUpdate();
+
+        #endregion
+        
+        /// <summary>
+        /// The callback function called when the weapon is put out.
+        /// This method should put out the weapon immediately.
+        /// </summary>
+        void OnPutOut();
+
+        /// <summary>
+        /// The async method to put out weapon.
+        /// Called when the weapon is put out.
+        /// Finish the async when the put out process complete (finish put out animation).
+        /// </summary>
+        /// <returns>Async Task</returns>
+        Task OnPutOutAsync();
+
+        /// <summary>
+        /// The callback method called when the weapon is put in.
+        /// </summary>
+        void OnPutIn();
+
+        /// <summary>
+        /// The async method to put in weapon.
+        /// Called when the weapon is put in.
+        /// Finish the async when the put in process complete (finish put in animation).
+        /// </summary>
+        /// <returns>Async Task</returns>
+        Task OnPutInAsync();
+
+        /// <summary>
+        /// Callback function called when the weapon trigger is pull down.
+        /// </summary>
+        void OnTriggerDown();
+
+        /// <summary>
+        /// Callback function called when the weapon trigger is up.
+        /// </summary>
+        void OnTriggerUp();
+    }
+}
---
+++ Runtime/IWeaponManager.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FinTOKMAK.WeaponSystem.Runtime
+{
+    /// <summary>
+    /// The interface of WeaponManager
+    /// </summary>
+    public interface IWeaponManager
+    {
+        #region Events
+
+        /// <summary>
+        /// The Action event triggered when the WeaponManager start initialization.
+        /// </summary>
+        Action onInitialize { get; }
+        
+        /// <summary>
+        /// The Action event triggered when the WeaponManager finish initialization.
+        /// </summary>
+        Action onFinishInitialize { get; }
+
+        #endregion
+        
+        /// <summary>
+        /// All the weapon current manager is carrying.
+        /// </summary>
+        List<IWeapon> carryWeapons { get; }
+
+        /// <summary>
+        /// Put out a weapon using the weapon index in the carryWeapon list.
+        /// When there's no weapon in the hand, the WeaponManager will put out weapon directly.
+        /// When there's already a weapon in hand, the WeaponManager will put in current using weapon first,
+        /// then put out the target weapon.
+        /// </summary>
+        /// <param name="index">the index of the weapon in the caryWeapons list, start from 0.</param>
+        void PutOut(int index);
+
+        /// <summary>
+        /// Put out a weapon using the weapon unique string id.
+        /// When there's no weapon in the hand, the WeaponManager will put out weapon directly.
+        /// When there's already a weapon in hand, the WeaponManager will put in current using weapon first,
+        /// then put out the target weapon.
+        /// </summary>
+        /// <param name="id">the unique string id of the weapon</param>
+        void PutOut(string id);
+
+        /// <summary>
+        /// Put out a weapon using the weapon object.
+        /// When there's no weapon in the hand, the WeaponManager will put out weapon directly.
+        /// When there's already a weapon in hand, the WeaponManager will put in current using weapon first,
+        /// then put out the target weapon.
+        /// </summary>
+        /// <param name="weapon">the weapon to put out.</param>
+        void PutOut(IWeapon weapon);
+
+        /// <summary>
+        /// The async version of method <see cref="PutOut(int)"/>
+        /// </summary>
+        /// <param name="index">the index of the weapon in the caryWeapons list, start from 0.</param>
+        /// <returns>Async Task</returns>
+        Task PutOutAsync(int index);
+        
+        /// <summary>
+        /// The async version of method <see cref="PutOut(string)"/>
+        /// </summary>
+        /// <param name="id">the unique string id of the weapon</param>
+        /// <returns>Async Task</returns>
+        Task PutOutAsync(string id);
+        
+        /// <summary>
+        /// The async version of method <see cref="PutOut(IWeapon)"/>
+        /// </summary>
+        /// <param name="weapon">the weapon to put out.</param>
+        /// <returns>Async Task</returns>
+        Task PutOutAsync(IWeapon weapon);
+
+        /// <summary>
+        /// Put in the current weapon.
+        /// </summary>
+        void PutIn();
+
+        /// <summary>
+        /// The async version of method <see cref="PutIn"/>
+        /// </summary>
+        /// <returns>Async Task</returns>
+        Task PutInAsync();
+
+
+        /// <summary>
+        /// The method to pull trigger down.
+        /// </summary>
+        void TriggerDown();
+
+        /// <summary>
+        /// The method to release the trigger.
+        /// </summary>
+        void TriggerUp();
+    }
+}
