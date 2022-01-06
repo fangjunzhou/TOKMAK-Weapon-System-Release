0f31b70871865ed2683a4927342750ceb0875b2f
Fangjun Zhou <fangjun.zhou02@gmail.com>
2022-01-05 19:56:06 -0600

    Added: Weapon class Inherit IWeapon interface

---
+++ Runtime/Weapon.cs
@@ -0,0 +1,56 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+
+namespace FinTOKMAK.WeaponSystem.Runtime
+{
+    [CreateAssetMenu(fileName = "Weapon", menuName = "FinTOKMAK/Weapon System/Weapon", order = 0)]
+    public class Weapon : ScriptableObject, IWeapon
+    {
+        public string id { get; }
+        
+        public void OnInitialize()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnUpdate()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnPutOut()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task OnPutOutAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnPutIn()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task OnPutInAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnTriggerDown()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnTriggerUp()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnReload()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
