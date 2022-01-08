8cb75accfec752f5363c7c12bfb8e706554810b0
Fangjun Zhou <fangjun.zhou02@gmail.com>
2022-01-08 02:23:44 -0600

    Update: WeaponManager Finish the implementation of all the methods.

--- Runtime/WeaponManager.cs
+++ Runtime/WeaponManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using UnityEngine;
 using Object = System.Object;
@@ -39,7 +40,7 @@ namespace FinTOKMAK.WeaponSystem.Runtime
         {
             get
             {
-                return _carryWeapons;
+                return _carryWeapons.Cast<IWeapon>().ToList();
             }
         }
 
@@ -68,12 +69,12 @@ namespace FinTOKMAK.WeaponSystem.Runtime
         /// <summary>
         /// All the weapons current WeaponManager is carrying.
         /// </summary>
-        private List<IWeapon> _carryWeapons;
+        private List<Weapon> _carryWeapons;
 
         /// <summary>
         /// The weapon player is currently using.
         /// </summary>
-        private IWeapon _currWeapon;
+        private Weapon _currWeapon;
 
         /// <summary>
         /// The current WeaponManager's state.
@@ -101,36 +102,103 @@ namespace FinTOKMAK.WeaponSystem.Runtime
             onFinishInitialize?.Invoke();
         }
 
+        private void Update()
+        {
+            // Call the update callback function of the current weapon.
+            if (_currWeapon != null)
+            {
+                _currWeapon.OnUpdate();
+            }
+        }
+
         #region IWeaponManager Interface
 
         public void PutOut(int index)
         {
-            throw new NotImplementedException();
+            // If there's still using weapon.
+            if (_currWeapon != null)
+            {
+                // Put in _currWeapon first
+                PutIn();
+            }
+            
+            // Put out the weapon directly
+            _carryWeapons[index].OnPutOut();
+            _currWeapon = _carryWeapons[index];
+            
+            _state = WeaponManagerState.Ready;
         }
 
         public void PutOut(string id)
         {
-            throw new NotImplementedException();
+            // Find the index of weapon with same id
+            for (int i = 0; i < carryWeapons.Count; i++)
+            {
+                if (carryWeapons[i].id == id)
+                {
+                    PutOut(i);
+                    return;
+                }
+            }
+
+            // No weapon found.
+            throw new InvalidOperationException($"No weapon with name {id}");
         }
 
         public void PutOut(IWeapon weapon)
         {
-            throw new NotImplementedException();
+            if (!_carryWeapons.Contains(weapon))
+                throw new InvalidOperationException($"No weapon {weapon}");
+
+            // Try to cast IWeapon to Weapon.
+            if (!(weapon is Weapon weapon1))
+                throw new InvalidCastException($"{weapon} is not a Weapon instance.");
+            
+            PutOut(_carryWeapons.IndexOf(weapon1));
         }
 
-        public Task PutOutAsync(int index)
+        public async Task PutOutAsync(int index)
         {
-            throw new NotImplementedException();
+            // If there's still using weapon.
+            if (_currWeapon != null)
+            {
+                // Put in _currWeapon first
+                await PutInAsync();
+            }
+
+            _state = WeaponManagerState.PuttingOut;
+            // Put out the weapon directly
+            await _carryWeapons[index].OnPutOutAsync();
+            _state = WeaponManagerState.Ready;
+            _currWeapon = _carryWeapons[index];
         }
 
-        public Task PutOutAsync(string id)
+        public async Task PutOutAsync(string id)
         {
-            throw new NotImplementedException();
+            // Find the index of weapon with same id
+            for (int i = 0; i < carryWeapons.Count; i++)
+            {
+                if (carryWeapons[i].id == id)
+                {
+                    await PutOutAsync(i);
+                    return;
+                }
+            }
+
+            // No weapon found.
+            throw new InvalidOperationException($"No weapon with name {id}");
         }
 
-        public Task PutOutAsync(IWeapon weapon)
+        public async Task PutOutAsync(IWeapon weapon)
         {
-            throw new NotImplementedException();
+            if (!_carryWeapons.Contains(weapon))
+                throw new InvalidOperationException($"No weapon {weapon}");
+
+            // Try to cast IWeapon to Weapon.
+            if (!(weapon is Weapon weapon1))
+                throw new InvalidCastException($"{weapon} is not a Weapon instance.");
+            
+            await PutOutAsync(_carryWeapons.IndexOf(weapon1));
         }
 
         public void PutIn()
@@ -167,12 +235,22 @@ namespace FinTOKMAK.WeaponSystem.Runtime
 
         public void TriggerUp()
         {
-            throw new NotImplementedException();
+            if (_currWeapon == null)
+            {
+                Debug.LogWarning("No weapon being used currently.");
+                return;
+            }
+            _currWeapon.OnTriggerUp();
         }
 
         public void Reload()
         {
-            throw new NotImplementedException();
+            if (_currWeapon == null)
+            {
+                Debug.LogWarning("No weapon being used currently.");
+                return;
+            }
+            _currWeapon.OnReload();
         }
 
         #endregion
