4143a5173122d30d6070d243d228807e78810aa3
Fangjun Zhou <fangjun.zhou02@gmail.com>
2022-01-17 15:19:33 -0600

    Added: IRemoteWeaponAgent Added agent for remote weapon control.

---
+++ Runtime/IRemoteWeaponAgent.cs
@@ -0,0 +1,21 @@
+﻿namespace FinTOKMAK.WeaponSystem.Runtime
+{
+    /// <summary>
+    /// The interface to call remote weapon RPC method.
+    /// </summary>
+    public interface IRemoteWeaponAgent<WeaponType> where WeaponType : IWeapon<WeaponType>
+    {
+        /// <summary>
+        /// The WeaponManager.
+        /// </summary>
+        IWeaponManager<WeaponType> manager { get; set; }
+
+        /// <summary>
+        /// The method to call the RPC on remote weapons.
+        /// </summary>
+        /// <param name="index">The index of the weapon.</param>
+        /// <param name="method">The method name.</param>
+        /// <param name="methodParams">The method parameters.</param>
+        void CallRPC(int index, string method, object[] methodParams);
+    }
+}
